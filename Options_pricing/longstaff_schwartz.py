b5622352c766b4a2efa73fabb4034df9283fa943
Saakaar Bhatnagar <40839313+Saakaarb@users.noreply.github.com>
2020-02-06 17:32:08 -0800

    Add files via upload

---
+++ Options_pricing/longstaff_schwartz.py
@@ -0,0 +1,133 @@
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+import random
+from copy import deepcopy
+def decision(probability):
+    return random.random() < probability
+
+
+
+
+def monte_carlo(T,dt,q,S0,increase,decrease):
+    # Runs a single monte-carlo simulation
+
+    #Inputs: T: Expiration time
+    #dt: Time step
+    #q: Risk Neutral Probability 
+    #S0: Initial Asset price
+
+    #outputs: Simulated asset price from one rollout
+    num_timesteps=int(T/dt);
+
+    SP=np.zeros([num_timesteps+1]);
+    SP[0]=S0;
+    for i_step in range(num_timesteps):
+        
+        if decision(q):
+            SP[i_step+1]=SP[i_step]*increase;
+        else:
+            SP[i_step+1]=SP[i_step]*decrease;
+    
+    return SP
+
+def create_simulatedpaths(T,dt,q,S0,increase,decrease,m):
+    
+    #Return square matrix of shape: (num simulated paths, num timesteps);
+    
+    #Inputs: T: Expiration time
+    #dt: Time step
+    #q: Risk Neutral Probability
+    #S0: Initial Asset price
+
+
+    num_timesteps=int(T/dt);
+    SP_main=np.zeros([m,num_timesteps+1]);
+    
+    for i_m in range(m):
+        SP_main[i_m,:]=deepcopy(monte_carlo(T,dt,q,S0,increase,decrease));
+    
+    return SP_main;
+    
+
+#def payoff(K,St):
+    # Payoff for a call option
+#    return max(St-K,0);
+
+
+def payoff(K,St):
+    # Payoff for a put option
+    return max(K-St,0);
+
+def feature_0(T,t):
+    return np.sin(np.pi*(T-t)/(2*T));
+def feature_1(T,t):
+    return np.log(T-t);
+def feature_2(T,t):
+    return (t/T)**2;
+
+def longstaff_schwartz(T,dt,q,S0,increase,decrease,m,r,K,num_features):
+
+    #Implementation of Longstaff-Schwartz Algorithm to optimally exercise American Options
+
+    #Inputs: T: Expiration time
+    #dt: Time step
+    #q: Risk Neutral Probability
+    #S0: Initial Asset price
+    #m: Number of Simulation paths to average over
+    #r: Rate of riskless return;
+    #K: Strike Price
+    SP=deepcopy(create_simulatedpaths(T,dt,q,S0,increase,decrease,m))
+    
+    discount_factor=np.exp(-r*dt);
+    num_timesteps=int(T/dt);
+    CF=np.zeros([m]);
+    X=np.zeros([m,num_features])
+    Y=np.zeros([m])
+    weights=np.zeros([num_features]);
+    for i in range(CF.shape[0]):
+
+        CF[i]=payoff(K,SP[i,-1]);# Expiration time option mandatory excercise payoff.
+    
+    for i_time in range(num_timesteps-1,0,-1): # Loop between times last, and first non inclusive
+        
+        CF=CF*discount_factor;
+        t=i_time*dt
+        for i_m in range(m):
+            
+            if payoff(K,SP[i_m,i_time])>0:
+                X[i_m,0]=feature_0(T,t);
+                X[i_m,1]=feature_1(T,t);
+                X[i_m,2]=feature_2(T,t);
+                Y[i_m]=CF[i];
+
+        weights=deepcopy(np.matmul(np.linalg.inv(np.matmul(np.transpose(X),X)),np.matmul(np.transpose(X),Y))); # Regression solution
+        
+        for i in range(m):
+
+            if payoff(K,SP[i,i_time])>np.sum(np.multiply(weights,X[i,:])): # If condition is true, option should be exercised
+                CF[i]=payoff(K,SP[i,i_time]);   # Cashflow in exercising option
+    exercise=payoff(K,S0);  # Payoff of immediate exercise
+    continue_val=discount_factor*np.mean(CF) # Expectation of cashflow
+
+    if exercise>continue_val:
+        return 1;
+    else:
+        return 0;
+
+    
+if __name__=="__main__":
+
+    T=1.5;
+    dt=0.5;
+    increase=1.2;
+    decrease=0.8;
+    m=5;
+    r=0.05;
+    K=100;
+    S0=100;
+    num_features=3;
+    discount_factor=np.exp(-r*dt);
+    q=(discount_factor**(-1)-decrease)/(increase-decrease)# Risk Neutral Probability measure, that stock increases in value
+    
+    current_action=longstaff_schwartz(T,dt,q,S0,increase,decrease,m,r,K,num_features)
